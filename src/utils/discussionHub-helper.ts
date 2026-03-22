a5c6651b7fe71f8e40d3108e51c9b7039a3904a3
rajeevsathish <rajeev.sathish@tarento.com>
2026-03-22 15:47:28 +0530

    KB-0000 removing the nodeBB code

--- src/utils/discussionHub-helper.ts
+++ src/utils/discussionHub-helper.ts
@@ -2,13 +2,13 @@
 // This file is kept as a placeholder to avoid breaking imports until all consumers are cleaned up.
 
 // tslint:disable-next-line: no-any
-export async function getUserUIDBySession(_req: any): Promise<string | undefined> {
-    return undefined
+export async function getUserUIDBySession(_req: any): Promise<string> {
+    return ''
 }
 
 // tslint:disable-next-line: no-any
-export async function getUserSlug(_req: any, _wid: any): Promise<string | undefined> {
-    return undefined
+export async function getUserSlug(_req: any, _wid: any): Promise<string> {
+    return ''
 }
 
 export function getWriteApiToken(): string {
